e4cd22e3069334ad09eb92d375eb268dba03b7d7
Haqve_vu <novruzluqyds20@gmail.com>
2023-12-07 14:51:18 -0800

    Add project files.

---
+++ ClientSide/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+
+var _CLIENT = new HttpClient();
+while (true)
+{
+    Console.Write("Write Letter: ");
+    var Letter = Console.ReadLine();
+
+
+    var result = await _CLIENT.GetStringAsync($"http://localhost:27001/?q={Letter}");
+
+    Console.WriteLine(result);
+}
---
+++ ServerSide/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System.Text.Json;
+
+// Cache
+Dictionary<string, int> Cache = new Dictionary<string, int>();
+
+// DataBase 
+string FilePath = "C:\\Users\\Elgun\\source\\repos\\Http_Project_2\\ServerSide\\Database.json";
+string JsonString = File.ReadAllText(FilePath);
+Dictionary<string, int> DataBase = JsonSerializer.Deserialize<Dictionary<string, int>>(JsonString);
+
+// Http Listener
+var url = "http://localhost:27001/";
+
+var _LISTENER = new HttpListener();
+_LISTENER.Prefixes.Add(url);
+_LISTENER.Start();
+
+// Listening
+while (true)
+{
+
+    var context = await _LISTENER.GetContextAsync();
+
+    var request = context.Request;
+    var response = context.Response;
+
+    response.Headers.Add("Content-Type", "text/plain");
+    response.StatusCode = (int)HttpStatusCode.OK;
+
+    var queryStringValue = request.QueryString["q"]?.ToString();
+
+    bool IsFound = false;
+    for (int i = 0; i < Cache.Count; i++)
+    {
+        if (Cache.ElementAt(i).Key == queryStringValue)
+        {
+            using (var sw = new StreamWriter(response.OutputStream))
+            {
+                await sw.WriteLineAsync($"From Cache -> {Cache.ElementAt(i).Key}: {Cache.ElementAt(i).Value}");
+            }
+            IsFound = true;
+            break;
+        }
+    }
+    if (!IsFound)
+    {
+        for (int i = 0; i < DataBase.Count; i++)
+        {
+            if (DataBase.ElementAt(i).Key == queryStringValue)
+            {
+                Cache.Add(DataBase.ElementAt(i).Key, DataBase.ElementAt(i).Value);
+                using (var sw = new StreamWriter(response.OutputStream))
+                {
+                    await sw.WriteLineAsync($"From Database -> {DataBase.ElementAt(i).Key}: {DataBase.ElementAt(i).Value}");
+                }
+                IsFound = true;
+                break;
+            }
+        }
+        if (!IsFound)
+        {
+            using (var sw = new StreamWriter(response.OutputStream))
+            {
+                await sw.WriteLineAsync($"Not Exist In Database ...");
+            }
+        }
+    }
+}
+
